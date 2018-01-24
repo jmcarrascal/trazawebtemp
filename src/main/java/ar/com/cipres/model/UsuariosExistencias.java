d3f12062b7d85026274edc8a2850c0b4fc537543
Juan Manuel Carrascal <juanmanuelcarrascal@MacBook-Pro-de-Juan-2.local>
2018-01-24 08:49:29 -0300

    s

--- src/main/java/ar/com/cipres/model/UsuariosExistencias.java
+++ src/main/java/ar/com/cipres/model/UsuariosExistencias.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "usuariosexistencias", schema = "")
+@Table(name = "usuariosexistencias", schema = "trazawebDistri.dbo")
 public class UsuariosExistencias implements java.io.Serializable {
 
 	@Id
